c3b4ee6cf38d08126e4f064fd121f449cb272dcd
Profisgolf <e5jrodrigu5@berchmans.edu.co>
2025-09-01 10:07:21 -0500

    Create exercise.py

---
+++ exercise.py
@@ -0,0 +1,10 @@
+#Luciana Salazar, Juan Felipe Rodriguez & Juan Martin Rivera 9A
+ 
+age = 25
+name = "Juan"
+last_name = "Pérez"
+counter = 0
+counter += 1
+message= name +" " + last_name 
+print (message)
+print("Counter:", counter)
